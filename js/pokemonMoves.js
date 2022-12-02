6f40beb32ecb8b6d39940d0d6f1a573d55827cbb
Felix Wu <2183614@jeaneudes.net>
2022-12-02 15:59:28 -0500

    pokemon stats and moves

---
+++ js/pokemonMoves.js
@@ -0,0 +1,199 @@
+//Grovyle and Sceptile
+const gigaDrain = {
+    name: "Giga Drain",
+    atk: 75,
+    acc: 100,
+    type: "grass"
+}
+
+const xScissor = {
+    name: "X-Scissor",
+    atk: 80,
+    acc: 100,
+    type: "bug"
+}
+
+const leer = {
+    name: "Leer",
+    atk: 0,
+    acc: 100,
+    defDebuff: 1,
+    type: "normal"
+}
+
+const quickAttack = {
+    name: "Quick Attack",
+    atk: 40,
+    acc: 100,
+    type: "normal"
+}
+
+//Combusken and Blaziken
+const featherDance = {
+    name: "Feather Dance",
+    atk: 0,
+    acc: 100,
+    atkDebuff: 2,
+    type: "flying"
+}
+
+const blazeKick = {
+    name: "Blaze Kick",
+    atk: 85,
+    acc: 90,
+    type:"fire"
+}
+
+const flameCharge = {
+    name: "Flame Charge",
+    atk: 50,
+    acc: 100,
+    speedBuff: 1,
+    type:"fire"
+}
+
+const doubleKick = {
+    name: "Double Kick",
+    atk: 70,
+    acc: 100,
+    type:"fighting"
+}
+
+//Marshtomp and Swampert
+const earthquake = {
+    name: "Earthquake",
+    atk: 100,
+    acc: 100,
+    type: "ground"
+}
+
+const amnesia = {
+    name: "Amnesia",
+    atk: 0,
+    acc: 100,
+    defBuff: 2,
+    type: "psychic"
+}
+
+const surf = {
+    name: "Surf",
+    atk: 90,
+    acc: 100,
+    type: "water"
+}
+
+const growl = {
+    name: "Growl",
+    atk: 0,
+    acc: 100,
+    atkDebuff: 1,
+    type: "normal"
+}
+
+//Poochyena
+const tackle = {
+    name: "Tackle",
+    atk: 40,
+    acc: 100,
+    type: "normal"
+}
+
+const howl = {
+    name: "Howl",
+    atk: 0,
+    acc: 100,
+    atkBuff: 1,
+    type: "normal"
+}
+
+//Mightyena
+const crunch = {
+    name: "Crunch",
+    atk: 80,
+    acc: 100,
+    type: "dark"
+}
+
+const howl = {
+    name: "Howl",
+    atk: 0,
+    acc: 100,
+    atkBuff: 1,
+    type: "normal"
+}
+
+const fireFang = {
+    name: "Fire Fang",
+    atk: 65,
+    acc: 95,
+    type: "fire"
+}
+
+const headbutt = {
+    name: "Headbutt",
+    atk: 70,
+    acc: 100,
+    type: "normal"
+}
+
+//Zigzagoon
+const tackle = {
+    name: "Tackle",
+    atk: 40,
+    acc: 100,
+    type: "normal"
+}
+
+const growl = {
+    name: "Growl",
+    atk: 0,
+    acc: 100,
+    atkDebuff: 1,
+    type: "normal"
+}
+
+//Taillow
+const peck = {
+    name: "Peck",
+    atk: 35,
+    acc: 100,
+    type: "flying"
+}
+
+const quickAttack = {
+    name: "Quick Attack",
+    atk: 40,
+    acc: 100,
+    debuff: 1,
+    type: "normal"
+}
+
+//Swellow
+const braveBird = {
+    name: "Brave Bird",
+    atk: 120,
+    acc: 100,
+    type: "flying"
+}
+
+const howl = {
+    name: "Howl",
+    atk: 0,
+    acc: 100,
+    debuff: 1,
+    type: "normal"
+}
+
+const quickAttack = {
+    name: "Quick Attack",
+    atk: 40,
+    acc: 100,
+    type: "normal"
+}
+
+const airSlash = {
+    name: "Air Slash",
+    atk: 75,
+    acc: 95,
+    type: "flying"
+}
