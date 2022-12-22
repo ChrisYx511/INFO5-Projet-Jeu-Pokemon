e8f0a662fa352454eae3983874b788706824c645
Chris Yang <58494380+ChrisYx511@users.noreply.github.com>
2022-12-22 00:26:47 -0500

    Mild changes in entrypoint

--- entrypoint.bat
+++ entrypoint.bat
@@ -1,7 +1,8 @@
+@echo off
 REM Ce fichier est utilisé pour configurer un serveur local pour permettre à certaines fonctions du jeu, tel le stockage local
 REM de fonctionner.
 REM Chris Yang, Felix Wu - Concentration Informatique 5
 
 SET PATH=%~dp0\node;%PATH%
-ECHO "Fermer cette fenêtre lorsque la consulation est terminée."
+ECHO Fermer cette fenêtre lorsque la consulation est terminée.
 npx live-server
